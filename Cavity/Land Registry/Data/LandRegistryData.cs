bcae4e5818ead3191f074aef4d1deba5e774c3e3
adean <alan.dean@gmail.com>
2014-01-23 10:04:35 +0000

    Added Cavity presentation & example.

---
+++ Cavity/Land Registry/Data/LandRegistryData.cs
@@ -0,0 +1,45 @@
+﻿namespace Example.Data
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using Cavity.Collections;
+    using Cavity.Data;
+    using Cavity.IO;
+
+    public sealed class LandRegistryData : DataSheet
+    {
+        public LandRegistryData(FileInfo source)
+        {
+            Source = source;
+        }
+
+        private FileInfo Source { get; set; }
+
+        protected override IEnumerator<T> GetEnumerator<T>()
+        {
+            return Entries<T>().GetEnumerator();
+        }
+
+        private IEnumerable<T> Entries<T>()
+            where T : KeyStringDictionary, new()
+        {
+            using (var temp = new CurrentTempDirectory())
+            {
+                var file = temp.Info.ToFile("data.csv");
+                using (var writers = new StreamWriterDictionary("Unique ID,Price,date,Postcode,Property type,old/new,Duration,PAON,SAON,Street,Locality,Town,District,County,Record Status"))
+                {
+                    foreach (var line in Source.Lines())
+                    {
+                        writers.Item(file).WriteLine(line);
+                    }
+                }
+
+                var transformer = new LandRegistryExtractionTransformer<T>();
+                foreach (var entry in new CsvDataSheet(file).Transform(transformer))
+                {
+                    yield return entry;
+                }
+            }
+        }
+    }
+}
---
+++ Cavity/Land Registry/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Example
+{
+    using System.IO;
+    using Cavity.IO;
+    using Example.Data;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            //// Download pp-complete.csv from http://www.landregistry.gov.uk/market-trend-data/public-data/price-paid-data
+            var download = new FileInfo(@"C:\Development.Temp\pp-complete.csv");
+
+            Aggregate(Extract(download));
+        }
+
+        private static void Aggregate(FileInfo source)
+        {
+            var destination = new FileInfo(@"C:\Development.Temp\aggregate.csv");
+
+            TempCsvFile.Create(new AveragePropertyPriceData(source), destination);
+        }
+
+        private static FileInfo Extract(FileInfo source)
+        {
+            var destination = new FileInfo(@"C:\Development.Temp\extract.csv");
+
+            TempCsvFile.Create(new LandRegistryData(source), destination);
+
+            return destination;
+        }
+    }
+}
